e7bef86fe3d182ce1aad49f9a3128daca70e9aad
Saayaman <ayakosayama1987@gmail.com>
2017-07-07 14:19:46 -0700

    Eatable

--- Practices/Equatable/Equatable/main.swift
+++ Practices/Equatable/Equatable/main.swift
@@ -8,26 +8,51 @@
 
 import Foundation
 
-print("Hello, World!")
-
-
 class Teacher: Equatable{
+
+
     var name:String
+    var age:Int
     
-    init(name: String) {
+    init(name: String, age:Int) {
         self.name = name
+        self.age = age
+    }
+    
+    //we compare because we cant.
+    static func ==(lhs: Teacher, rhs: Teacher) -> Bool{
+        return lhs.name == rhs.name && lhs.age == rhs.age
     }
 }
 
+var teachers:[Teacher] = []
 
-func ==(lhs: Teacher, rhs: Teacher) -> Bool{
-    return lhs.name == rhs.name
+var one = Teacher(name:"AJ", age:20)
+var two = Teacher(name: "Bob", age:30)
+var three = Teacher(name: "Amy", age: 50)
+var four = Teacher(name: "John", age:30)
+var five = Teacher(name: "AJ", age: 20)
+
+teachers.append(one)
+teachers.append(two)
+teachers.append(three)
+teachers.append(four)
+
+
+
+if one.name == two.name{
+    print("match!")
 }
 
+if one == two{
+    print("same!")
+}
 
-var one = Teacher(name:"AJ")
-var two = Teacher(name: "Bob")
+if teachers[0] == teachers[1]{
+    
+}
 
 
 print(one == two)
 print(one != two)
+print(one == five)
